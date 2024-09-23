796c6af45d6773aaced4271dfe8a23db2d2c843f
eduardo galeas <ei.galeas@uea.edu.ec>
2024-09-22 23:14:04 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SEMANA11/buscar_arreglo.py

--- SEMANA11/buscar_arreglo.py
+++ SEMANA11/buscar_arreglo.py
@@ -1,23 +1,17 @@
-#datod de las temperaturas
-datos_temperatura = {
-    [#"Ciudad Loja"
-        [3,12,23,10], # 0
-        [31, 11,19,14] # 1
-        [12,15,14,9] # 2
-        [7,10,3,1] # 3
-     ],
-    [#"Ciudad MACHALA"
-        [13,22,3,1], # 4
-        [7, 10,9,17], # 5
-        [11,5,13,29], # 6
-        [17,10,23,16], # 7
-     ],
-    [#"Ciudad Zamora"
-        [5,18,33,10], # 8
-        [3, 1,9,14], # 9
-        [2,15,14,7], # 10
-        [37,10,23,11], # 11
-     ],
+# Se crea la función
+def calcular_descuento(monto_total, porcentaje_descuento=10):
+    descuento = (porcentaje_descuento / 100) * monto_total
+    return descuento
+
+# Llamar a la función
+monto1 = 20000  # Primer monto total de compra
+descuento1 = calcular_descuento(monto1)  # valor predeterminado del 10%
+print(f"Descuento de la primera compra: {descuento1}")
+# segundo descuento
+monto2 = 30000  # Segundo monto total de compra
+porcentaje_descuento2 = 10  # Porcentaje de descuento personalizado
+descuento2 = calcular_descuento(monto2, porcentaje_descuento2)
+print(f"Descuento de la segunda compra: {descuento2}")
 
 
 
