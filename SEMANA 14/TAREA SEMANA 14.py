b0f072474b1ddd60a804e6825c8abca410797dc4
eduardo galeas <ei.galeas@uea.edu.ec>
2024-10-05 16:23:03 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ SEMANA 14/TAREA SEMANA 14.py
@@ -0,0 +1,18 @@
+def calcular_descuento(monto_total, porcentaje_descuento=10):
+    # Calcular el descuento
+    descuento = monto_total * (porcentaje_descuento / 100)
+    return descuento
+# Se crea la función
+def calcular_descuento(monto_total, porcentaje_descuento=10):
+    descuento = (porcentaje_descuento / 100) * monto_total
+    return descuento
+
+# Llamar a la función
+monto1 = 55000  # Primer monto total de compra
+descuento1 = calcular_descuento(monto1)  # valor predeterminado del 10%
+print(f"Descuento de la primera compra: {descuento1}")
+# segundo descuento
+monto2 = 75000  # Segundo monto total de compra
+porcentaje_descuento2 = 10  # Porcentaje de descuento personalizado
+descuento2 = calcular_descuento(monto2, porcentaje_descuento2)
+print(f"Descuento de la segunda compra: {descuento2}")
