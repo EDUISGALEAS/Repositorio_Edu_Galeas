414e10cbe5ae47b0f1e80c94a531e223be765de4
eduardo galeas <ei.galeas@uea.edu.ec>
2024-10-06 00:20:58 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ SEMANA 16/TAREA SEMANA 16.py
@@ -0,0 +1,28 @@
+# Crear el archivo
+archivo = open("my_notes.txt", "w")
+notas = [
+    "mañana toca ir a trabajar otra vez.\n",
+    "quiero vacaciones urgente.\n",
+    "FELICITAR A EDU GALEAS.\n"
+]
+
+for nota in notas:
+    archivo.write(nota)
+
+archivo.close()
+
+
+# Leer y mostrar el contenido del archivo
+archivo = open("my_notes.txt", "r")
+linea = archivo.readline()
+
+while linea:
+    print(linea, end="")
+    linea = archivo.readline()
+
+archivo.close()
+
+
+
+
+
